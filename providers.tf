541c7e4bf7f9c469ce6a36b98f803d25cba049db
Nathan Smith <nathan@chainguard.dev>
2022-07-18 14:36:43 -0700

    Fix formatting

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google-beta = {
-      source  = "hashicorp/google-beta"
+      source = "hashicorp/google-beta"
     }
     google = {
       source = "hashicorp/google"
