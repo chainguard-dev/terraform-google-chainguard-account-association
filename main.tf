21f3068ec91dabd0f2b00c39bd83413cab31eb33
Matt Moore <mattmoor@chainguard.dev>
2024-06-09 10:31:06 -0700

    Hollow out the account associations repo
    This is in preparation to bring it back _in force_ (pun intended).
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-data "google_project" "provider_default" {
-}
-
-locals {
-  project_id        = var.google_project_id == "" ? data.google_project.provider_default.project_id : var.google_project_id
-  enforce_group_ids = length(var.enforce_group_ids) > 0 ? var.enforce_group_ids : [var.enforce_group_id]
-}
-
 // Providers define the scopes at which Chainguard services may impersonate
 // the service accounts associated with the enclosing pool.
 // The "allowed_audiences" field below should encode the set of Chainguard
@@ -14,12 +6,12 @@ locals {
 // Provision the provider (under the pool) that supports mapping identity tokens
 // for this environment of Chainguard.
 resource "google_iam_workload_identity_pool_provider" "chainguard_provider" {
-  project                            = local.project_id
+  project                            = var.project_id
   provider                           = google-beta
   workload_identity_pool_id          = google_iam_workload_identity_pool.chainguard_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "chainguard-provider" # This gets 4-32 alphanumeric characters (and '-')
   display_name                       = "Chainguard provider"
-  description                        = "This is the provider for impersonation by the ${var.enforce_domain_name} Chainguard environment's issuer for ${var.enforce_domain_name}."
+  description                        = "This is the provider for impersonation by Chainguard services (environment: ${var.environment})."
 
   attribute_mapping = {
     "google.subject" = "assertion.sub"
@@ -27,13 +19,13 @@ resource "google_iam_workload_identity_pool_provider" "chainguard_provider" {
   }
   oidc {
     allowed_audiences = ["google"]
-    issuer_uri        = "https://issuer.${var.enforce_domain_name}"
+    issuer_uri        = "https://issuer.${var.environment}"
   }
 }
 
 // Enable the API to support impersonation.
 resource "google_project_service" "iamcredentials-api" {
-  project                    = local.project_id
+  project                    = var.project_id
   service                    = "iamcredentials.googleapis.com"
   disable_dependent_services = false
   disable_on_destroy         = false
@@ -41,7 +33,7 @@ resource "google_project_service" "iamcredentials-api" {
 
 // Provision the Workload Identity Pool
 resource "google_iam_workload_identity_pool" "chainguard_pool" {
-  project                   = local.project_id
+  project                   = var.project_id
   provider                  = google-beta
   workload_identity_pool_id = "chainguard-pool"
   display_name              = "Chainguard Pool"
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "provider_id" {
+  description = "GCP identity provider pool configured for Chainguard."
   value       = google_iam_workload_identity_pool_provider.chainguard_provider.name
-  description = "GCP identity provider pool configured for Enforce"
 }
